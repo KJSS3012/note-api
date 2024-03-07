f6c8d6258b4c617c4b0148e55988037c43b55b7e
Kaique Santos <kaique30122004@gmail.com>
2024-03-07 15:33:14 -0300

    add simple cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
+const cors = require("cors");
 
 require("./config/database");
 
@@ -13,6 +14,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors({ origin: "http://localhost:3000" }));
 
 app.use("/users", usersRouter);
 app.use("/notes", notesRouter);
